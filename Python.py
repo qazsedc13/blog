36c0e843c9243c2b5f08c313b35f63d8e221627c
unknown <C:\Users\kolya\AppData\Roaming\The Bat!>
2015-12-20 18:50:03 +0600

    попробовали разные действия со строками

--- Python.py
+++ Python.py
@@ -14,3 +14,38 @@ Nikolay
 >>> name
 'Nikolay'
 >>> 
+per1=20
+>>> per2=30
+>>> per3=per1+per2
+>>> per3
+50
+>>> per1=47
+>>> per3
+50
+>>> a=1;b=2;print(a,b)
+1 2
+>>> # комментирование знаком
+>>> per = 'Any string'
+>>> per[2]
+'y'
+>>> per[2:7]
+'y str'
+>>> per[:5]
+'Any s'
+>>> per[:7]
+'Any str'
+>>> per[:-1]#срез кроме 1 последнего символа
+'Any strin'
+>>> len(per)# считает количество символов
+10
+>>> '123 '+'456'
+'123 456'
+>>> #Была операция склеивания
+>>> '123'*3#дублирование
+'123123123'
+>>> import keyword
+>>> keyword.kwlist
+['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+>>> keyword.iskeyword('except')#проверка есть ли это слово в зарезерврованных
+True
+>>> 
