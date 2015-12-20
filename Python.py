a7c338493505e0a6f25aabbdce22701c13c3580a
unknown <C:\Users\kolya\AppData\Roaming\The Bat!>
2015-12-20 18:09:35 +0600

    попробовали ввести переменную, попробовали сложение и умножение

--- Python.py
+++ Python.py
@@ -2,4 +2,15 @@ Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (In
 Type "copyright", "credits" or "license()" for more information.
 >>> print ("Hello World!!!")
 Hello World!!!
+>>> print (4+7)
+11
+>>> print(7*8)
+56
+>>> name = input("Your name? \n")
+Your name? 
+Nikolay
+>>> name
+'Nikolay'
+>>> name
+'Nikolay'
 >>> 
