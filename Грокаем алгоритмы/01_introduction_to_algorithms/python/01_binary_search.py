f4633294cd2a60796cdd392dbc8f3ddcad06bd06
Nikolay <bgnikkov@rambler.ru>
2019-12-15 11:57:34 +0700

    Переведены комментарии к функции
    Добавил комментарии на русском к функции.
    Принцип работы функции в отбрасывании половины не правильных цифр.
    То есть при поиске числа от 1 до 100 начинаем с 50, если число должно быть больше, то смотрим 75, если число должно быть меньше то 25 и так далее.
    Таким образом отбрасывается лишний диапазон и поиск происходит быстрее

--- Грокаем алгоритмы/01_introduction_to_algorithms/python/01_binary_search.py
+++ Грокаем алгоритмы/01_introduction_to_algorithms/python/01_binary_search.py
@@ -1,28 +1,36 @@
 def binary_search(list, item):
   # low and high keep track of which part of the list you'll search in.
+  # низкий и высокий следите за тем, какую часть списка вы будете искать в.
   low = 0
   high = len(list) - 1
 
   # While you haven't narrowed it down to one element ...
+  # Пока вы не сузили это до одного элемента ...
   while low <= high:
     # ... check the middle element
+    # ... проверить средний элемент
     mid = (low + high) // 2
     guess = list[mid]
     # Found the item.
+    # Найден предмет.
     if guess == item:
       return mid
     # The guess was too high.
+    # Предположение было слишком высоким.
     if guess > item:
       high = mid - 1
     # The guess was too low.
+    # Предположение было слишком низким.
     else:
       low = mid + 1
 
   # Item doesn't exist
+  # Элемент не существует
   return None
 
 my_list = [1, 3, 5, 7, 9]
 print(binary_search(my_list, 3)) # => 1
 
 # 'None' means nil in Python. We use to indicate that the item wasn't found.
+# «Нет» означает ноль в Python. Мы используем, чтобы указать, что предмет не был найден.
 print(binary_search(my_list, -1)) # => None
