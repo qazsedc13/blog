8c9d2eac3887c681b61a09303eac9d4f444179a6
Nikolay <bgnikkov@rambler.ru>
2019-12-16 21:03:31 +0700

    Сортировка выбором
    К функции прописал комментарии на русском. При создании нового массива значение из старого удаляется, после чего снова ищется минимальное из обновлённого старого массива

--- Грокаем алгоритмы/02_selection_sort/python/01_selection_sort.py
+++ Грокаем алгоритмы/02_selection_sort/python/01_selection_sort.py
@@ -1,8 +1,11 @@
 # Finds the smallest value in an array
+# Находим наименьшее значение в массиве
 def findSmallest(arr):
   # Stores the smallest value
+  # Хранит наименьше
   smallest = arr[0]
   # Stores the index of the smallest value
+  # Хранит индекс наименьшего значения
   smallest_index = 0
   for i in range(1, len(arr)):
     if arr[i] < smallest:
@@ -11,11 +14,15 @@ def findSmallest(arr):
   return smallest_index
 
 # Sort array
+# Сортировка массива
 def selectionSort(arr):
   newArr = []
   for i in range(len(arr)):
       # Finds the smallest element in the array and adds it to the new array
+      # Находим наименьший элемент в массиве и добавляем его в новый массив
       smallest = findSmallest(arr)
+      # добавляем наименьшее значение в новый массив, при этом удаляя это 
+      # значение из старого, после чего начинаем поиск заново
       newArr.append(arr.pop(smallest))
   return newArr
 
