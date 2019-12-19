77812f2dc4d6c3f6028c2d1039ac7ed8de93cb44
Nikolay <bgnikkov@rambler.ru>
2019-12-19 20:55:28 +0700

    быстрая сортировка
    добавлены комментарии на русском

--- Грокаем алгоритмы/04_quicksort/python/05_quicksort.py
+++ Грокаем алгоритмы/04_quicksort/python/05_quicksort.py
@@ -1,13 +1,16 @@
 def quicksort(array):
   if len(array) < 2:
     # base case, arrays with 0 or 1 element are already "sorted"
+	# базовый случай, массивы с 0 или 1 элементом уже "отсортированы"
     return array
   else:
     # recursive case
+	# рекурсивный случай
     pivot = array[0]
     # sub-array of all the elements less than the pivot
+	# вложенный массив всех элементов меньше, чем pivot
     less = [i for i in array[1:] if i <= pivot]
-    # sub-array of all the elements greater than the pivot
+    # вложенный массив всех элементов больше, чем pivot
     greater = [i for i in array[1:] if i > pivot]
     return quicksort(less) + [pivot] + quicksort(greater)
 
