a69423d536a5474389eea0047dd1672a502ff872
Nikolay <bgnikkov@rambler.ru>
2019-12-22 13:06:26 +0700

    Словарь
    Создали словарь (он же хеш-таблица). Внесли несколько значений, вывели цену одного продукта время выполнения O(1)

---
+++ Грокаем алгоритмы/05_hash_tables/python/01_price_of_groceries_0.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 22 13:00:19 2019
+
+@author: Николай
+"""
+
+book = dict()
+book['apple'] = 0.67
+book['milk'] = 1.49
+book['avocado'] = 1.67
+print(book['milk'])
